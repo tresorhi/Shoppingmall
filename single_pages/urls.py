1ecdc9e704fda41e484ffa1910ccb9a2d8001ab8
Jeong Hi <tresorhi@naver.com>
2022-12-18 03:01:29 +0900

    url 설정

---
+++ single_pages/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [  # IP주소/
+    path('', views.homepage),
+    path('about_company/', views.company)
+]
--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+#from blog.models import Post
 
 # Create your views here.
+def homepage(request):
+    #recent_post = Post.objects.order_by('-pk')[:3]
+    return render(request, 'single_pages/homepage.html')
+    #{'recent_posts' : recent_post,} 이게 html' 뒤에 콤마 찍고 있음
+def company(request):
+    return render(request, 'single_pages/company.html')
