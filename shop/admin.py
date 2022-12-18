2411a9267e8abf447ce2b7af3df1481f822d8aa0
Jeong Hi <tresorhi@naver.com>
2022-12-18 14:26:14 +0900

    상품, 댓글 모델 생성

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Tag, Manufacturer
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    preserve_filters = {'slug' : ('name',)}
+
+admin.site.register(Tag, TagAdmin)
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name',)}
+
+admin.site.register(Manufacturer, ManufacturerAdmin)
--- shop/models.py
+++ shop/models.py
@@ -44,3 +44,59 @@ class Manufacturer(models.Model):
 
     class Meta:
         verbose_name_plural = 'Manufacturers'
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    hook_text = models.CharField(max_length=100, blank=True)
+
+
+    head_image = models.ImageField(upload_to='shop/images/%Y/%m/%d/', blank=True)
+    # %Y 2022, %y 22
+
+    price = models.IntegerField(default=1)
+    scissors = models.IntegerField(default=10)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    #다대일 관계
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+    manufacturer = models.ForeignKey(Manufacturer, null=True, blank=True, on_delete=models.SET_NULL)
+
+    #다대다 관계
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.name}::{self.author} : {self.created_at}'
+
+    def get_absolute_url(self):
+        return f'/shop/{self.pk}/'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return 'https://dummyimage.com/50x50/ced4da/6c757d.jpg'
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author} : {self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.product.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return 'https://dummyimage.com/50x50/ced4da/6c757d.jpg'
